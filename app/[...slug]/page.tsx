674256ad5fbdd31db82355ce631091246f20e65a
KarolKonopka <karolkonopka98@gmail.com>
2023-06-29 20:19:44 +0200

    Adjust margins and padding for posts and home page Introduce title and description for home page

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -51,9 +51,13 @@ export default async function PagePage({ params }: PageProps) {
 
   return (
     <article className="py-6 prose dark:prose-invert">
-      <h1>{page.title}</h1>
-      {page.description && <p className="text-xl">{page.description}</p>}
-      <hr />
+      <h1 className="mb-2">{page.title}</h1>
+      {page.description &&
+        <p className="text-xl mt-0 text-neutral-700 dark:text-neutral-200">
+          {page.description}
+        </p>
+      }
+      <hr className="my-4" />
       <Mdx code={page.body.code} />
     </article>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,16 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <div className="prose dark:prose-invert">
+    <div className="py-6 prose dark:prose-invert">
+      <h1 className="mb-2">Home</h1>
+        <p className="text-xl mt-0 text-neutral-700 dark:text-neutral-200">
+          Welcome to my website!
+        </p>
+      <hr className="my-4" />
       {allPosts.map((post) => (
-        <article key={post._id}>
+        <article className="pb-4" key={post._id}>
           <Link href={post.slug}>
-            <h2>{post.title}</h2>
+            <h2 className="my-0">{post.title}</h2>
           </Link>
           {post.description && <p>{post.description}</p>}
         </article>
