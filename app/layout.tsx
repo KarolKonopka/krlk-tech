b92eb50a583f4dc9b833a30ff7a7fd014e3e46ca
KarolKonopka <karolkonopka98@gmail.com>
2023-06-29 13:33:17 +0200

    Update header

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,12 +25,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <div className="max-w-2xl mx-auto py-10 px-4">
             <header>
               <div className="flex items-center justify-between">
-                <ModeToggle />
-                <nav className="ml-auto text-sm font-medium space-x-6">
-                  <Link href="/">Home</Link>
+                <Link href="/"><span className="text-sm font-medium">KrlK.tech</span></Link>
+                <nav className="ml-auto text-sm font-medium space-x-6 mr-6">
                   <Link href="/projects">Projects</Link>
                   <Link href="/about">About</Link>
                 </nav>
+                <ModeToggle />
               </div>
             </header>
             <main>{children}</main>
